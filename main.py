0148a35102a70f46a844247b7621ef8027e29ccb
oğuz <84834653+softliner55@users.noreply.github.com>
2026-03-05 23:40:15 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:39:33.711Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T20:40:15.484Z
+Commit: Fix broken links in documentation

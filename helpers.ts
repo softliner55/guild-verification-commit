cc17e5f4ff9e9f57fd093babc382575c82fb65ed
oğuz <84834653+softliner55@users.noreply.github.com>
2026-03-05 23:36:20 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:35:12.424Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:36:19.843Z
+Commit: Fix broken links in documentation

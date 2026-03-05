6a8e7369607a5f4c442a9e50a12a87e8c836bfbe
oğuz <84834653+softliner55@users.noreply.github.com>
2026-03-05 23:40:30 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:40:25.579Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T20:40:30.336Z
+Commit: Patch security vulnerability in authentication
